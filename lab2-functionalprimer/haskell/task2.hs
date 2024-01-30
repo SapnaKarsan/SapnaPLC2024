576cc71eaab524df152a94339ec242e9b9fc98a5
Sapna Karsan <220090438@aston.ac.uk>
2024-01-30 11:55:02 +0000

    task2.hs

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,6 +1,6 @@
 onePlusOne = "1 + 1 = 2"
 
-sgn x = if x < 0 then -1 else if x == 0 then 0 else 1
+
     
 
 main = do
@@ -8,6 +8,3 @@ main = do
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
     putStrLn(onePlusOne)
-    input <- getLine
-    let x = (read input :: Int)
-    putStrLn( show (sgn (x)) )
