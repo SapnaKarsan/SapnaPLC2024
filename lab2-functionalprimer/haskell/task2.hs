1bdca32e9afe036330c3c6c3bb9ec33a9e8e7461
Sapna Karsan <220090438@aston.ac.uk>
2024-01-30 11:51:33 +0000

    task2 lab changes

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,13 @@
+onePlusOne = "1 + 1 = 2"
+
+sgn x = if x < 0 then -1 else if x == 0 then 0 else 1
+    
+
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    putStrLn(onePlusOne)
+    input <- getLine
+    let x = (read input :: Int)
+    putStrLn( show (sgn (x)) )
