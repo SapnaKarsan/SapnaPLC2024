85105ca6d78cddd7ca316ea2dabfa30b539fbcb5
Sapna Karsan <220090438@aston.ac.uk>
2024-01-30 11:32:27 +0000

    task1.hs lab

--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -3,9 +3,12 @@ module Main where
 main =
     do
     print (myFunc1 5)
-    --print (MyFunc2 10)
-    --print (3rdFunc)
+    print (myFunc2 10)
+    print (l3rdFunc)
 
+myFunc1 :: Int -> Int
+myFunc2 :: Int -> Int
+l3rdFunc :: String -> String
 myFunc1 x = x*10
---MyFunc2 x = x*2
---3rdFunc = "Hello, this is 3rd Func"
+myFunc2 x = x*2
+l3rdFunc = "Hello, this is 3rd Func"
